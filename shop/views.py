fdb5d70aa634b156675f729b1c9ea2ed02dc477e
shubham bairagi <sbairagi825@gmail.com>
2020-01-25 23:40:23 +0530

    create alert in contact page for user 'your Form has been submitted'

--- shop/views.py
+++ shop/views.py
@@ -29,6 +29,7 @@ def about(request):
     return render(request,'shop/about.html')
 
 def contact(request):
+    thank = False
     if request.method == 'POST':
         name = request.POST.get('name','')
         email = request.POST.get('email','')
@@ -36,7 +37,8 @@ def contact(request):
         desc = request.POST.get('desc','')
         contact = Contact(name=name,email=email,phone=phone,desc=desc)
         contact.save()
-    return render(request,'shop/contact.html')
+        thank = True
+    return render(request,'shop/contact.html',{'thank': thank})
 
 def tracker(request):
     if request.method == 'POST':
