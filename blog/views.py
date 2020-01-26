d771d95ed7e62c5f40d870befddee820db89275b
shubham bairagi <sbairagi825@gmail.com>
2020-01-26 21:03:44 +0530

    create blog page and blogpost page and update urls paterns and views

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'blog/index.html')
+
+
+def blogpost(request):
+    return render(request,'blog/blogpost.html')
