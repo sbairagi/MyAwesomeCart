cc0a2c06bf1a192add21089eace1f51dbfca6778
shubham bairagi <sbairagi825@gmail.com>
2020-01-27 19:06:51 +0530

    create blogpost.html page

--- blog/views.py
+++ blog/views.py
@@ -12,4 +12,4 @@ def index(request):
 def blogpost(request,id):
     post = Blogpost.objects.filter(post_id=id)[0]
     print(post)
-    return render(request,'blog/blogpost.html')
+    return render(request,'blog/blogpost.html',{'post':post})
