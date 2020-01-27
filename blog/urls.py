01e8b3e2de4327210b4d09b9ce49b7b3567e962a
shubham bairagi <sbairagi825@gmail.com>
2020-01-28 01:10:35 +0530

    add facebook page like, share, comment plugin in blog aap

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name="blogHome"),
-    path('blogpost/<int:id>', views.blogpost, name="blogPost"),
+    path('blogposts/<int:id>', views.blogpost, name="blogPost"),
 ]
