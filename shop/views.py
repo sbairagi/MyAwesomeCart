7e7953750762b226cefce731b3687ce037f17ae2
shubham bairagi <sbairagi825@gmail.com>
2020-01-28 16:55:29 +0530

    create search function and search page

--- shop/views.py
+++ shop/views.py
@@ -29,8 +29,36 @@ def index(request):
     #             [products, range(1, nSlides), nSlides]]
     params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
+def searchmatch(query, item):
+    '''return  true only if query matches the item'''
+    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
+def search(request):
+    query = request.GET.get('search')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod = [item for item in prodtemp if searchmatch(query, item)]
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod) != 0:
+            allProds.append([prod, range(1, nSlides), nSlides])
+
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds': allProds, "msg":""}
+    if len(allProds) == 0 or len(query)<3 :
+        params = {'msg': "Please make sure to enter relevent search query"}
+    return render(request, 'shop/search.html', params)
 
 def about(request):
+
     return render(request,'shop/about.html')
 
 def contact(request):
@@ -64,9 +92,7 @@ def tracker(request):
             return HttpResponse('{}')
     return render(request,'shop/tracker.html')
 
-def search(request):
 
-    return render(request,'shop/search.html')
 
 def prodview(request,myid):
     # featch the product using the id
